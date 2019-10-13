d5c3ba1fe9e647c544cdcc34a342adb68112478a
Foxclip <destroyApp2@gmail.com>
2019-10-13 20:14:25 +1000

    Limited amount of points in the plot

--- plot.py
+++ plot.py
@@ -3,7 +3,13 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
 
-plot_queue = None
+MAX_POINTS = 1000     # limiting amount of displayed points for performance purposes
+DECREASE_FACTOR = 10  # if MAX_POINTS is reached, point list is shrunk by this factor
+
+max_index = 1         # index of next point
+add_every = 1         # add point every x frames
+
+plot_queue = None     # multiprocessing.queue from which new points come
 
 
 def start(queue):
@@ -21,12 +27,31 @@ def init():
 
 
 def update(frame):
+
     while not plot_queue.empty():
-        xdata.append(len(xdata) + 1)
-        ydata.append(plot_queue.get())
-        ln.set_data(xdata, ydata)
-        ax.set_xlim(0, max(len(xdata), 1))
+
+        global xdata
+        global ydata
+        global max_index
+        global add_every
+
+        if max_index % add_every == 0:
+
+            if len(xdata) >= MAX_POINTS:
+                xdata = xdata[0::DECREASE_FACTOR]
+                ydata = ydata[0::DECREASE_FACTOR]
+                add_every *= DECREASE_FACTOR
+
+            xdata.append(max_index)
+            ydata.append(plot_queue.get())
+            ln.set_data(xdata, ydata)
+
+        else:
+            plot_queue.get()
+
+        ax.set_xlim(0, max_index)
         ax.set_ylim(0, max(ydata))
+        max_index += 1
 
 
 def anim(name, queue):
